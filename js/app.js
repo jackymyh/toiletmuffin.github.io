b39ee5a5407e8371e21e7c6941cc4ec08e99de17
toiletmuffin <jimmy_blackjack@hotmail.com>
2018-01-22 19:30:25 -0800

    scrolling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
